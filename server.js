f7b60ce3576c23db9341e5b91ac9e3022598f5fe
momair1131 <m.omair1131@gmail.com>
2023-10-10 16:32:04 -0400

    try again

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ const port = process.env.PORT;
 const cors = require("cors");
 
 const mongoURI = process.env.MONGO_URI;
-
+console.log(mongoURI);
 app.use(cors());
 
 mongoose.set("debug", true); // display mongoose query inside log
 
-mongoose.connect(mongoURI);
-mongoose.connection.once("open", () => {
-  console.log("connected to MongoDB");
+mongoose.connect(mongoURI).then(() => {
+  console.log("connected to mongodb");
 });
+// mongoose.connection.once("open", () => {
+//   console.log("connected to MongoDB");
+// });
 
 app.use(express.json()); // We use the express.json built-in middleware function to JSON content received from the incoming requests.
 
